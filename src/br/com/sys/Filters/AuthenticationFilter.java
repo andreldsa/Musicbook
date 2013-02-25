c70e07da6bd4ad5a56d606d61a653cebc424db65
Andre Abrantes <andredossantosabrantes@gmail.com>
2013-02-25 11:59:03 -0300

    include lib easyaccept; fix AuthenticationFilter

--- src/br/com/sys/Filters/AuthenticationFilter.java
+++ src/br/com/sys/Filters/AuthenticationFilter.java
@@ -2,6 +2,16 @@ package br.com.sys.Filters;
 
 import java.io.IOException;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import br.com.sys.beans.MainBean;
 
 public class AuthenticationFilter implements Filter{
