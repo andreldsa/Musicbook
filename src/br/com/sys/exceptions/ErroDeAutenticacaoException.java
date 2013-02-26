61dc62a76c25cbe85352437cca72337e1ca44483
Andre Abrantes <andredossantosabrantes@gmail.com>
2013-02-26 00:37:05 -0300

    deleted package src/br

--- src/br/com/sys/exceptions/ErroDeAutenticacaoException.java
+++
@@ -1,9 +0,0 @@
-package br.com.sys.exceptions;
-
-public class ErroDeAutenticacaoException extends Exception {
-	
-	public static final String MENSAGEM = "Erro de autenticação.";
-
-	private static final long serialVersionUID = -4189248068246593805L;
-	
-}
